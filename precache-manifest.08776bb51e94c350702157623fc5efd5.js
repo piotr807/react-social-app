49ad5b7703d3e7fedf40dbd89e66c8d38d74dead
Piotr Kolaniak <piokol807@interia.pl>
2020-10-05 12:34:05 +0200

    Updates

---
+++ precache-manifest.08776bb51e94c350702157623fc5efd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c790090023d9fd4bd3bbce4abaabe83",
+    "url": "/react-social-app/index.html"
+  },
+  {
+    "revision": "b5b85dcd88b5a852b7a8",
+    "url": "/react-social-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e65374499749d88b7d10",
+    "url": "/react-social-app/static/js/2.578a9e9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-social-app/static/js/2.578a9e9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5b85dcd88b5a852b7a8",
+    "url": "/react-social-app/static/js/main.f91b64b0.chunk.js"
+  },
+  {
+    "revision": "2c09fee4c0f08a8f63b5",
+    "url": "/react-social-app/static/js/runtime-main.fa0ccd24.js"
+  }
+]);
